faa4c02595feaef7ea7ffe68d45b2f27c7d8c566
Sofer Athlan-Guyot <chem@sathlan.org>
2016-07-23 17:41:16 +0200

    Check github.com in known_hosts.

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -1,6 +1,6 @@
 def call(String remoteUrl, String branch, String credentialsId, String repoName = 'origin') {
     sshagent([credentialsId]) {
-      sh("ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts")
+      sh("grep -q github.com ~/.ssh/known_hosts || ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts")
       sh("git remote -v | grep -q ${repoName} || git remote add ${repoName} ${remoteUrl}")
       sh("git -c core.askpass=true push ${repoName}")
     }
