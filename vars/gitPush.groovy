3adb35bc4922edcba71e50465effb9bac8de9615
Sofer Athlan-Guyot <chem@sathlan.org>
2016-07-23 17:40:41 +0200

    Add a verification before adding repo to remote.

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -1,7 +1,7 @@
 def call(String remoteUrl, String branch, String credentialsId, String repoName = 'origin') {
     sshagent([credentialsId]) {
       sh("ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts")
-      sh("git remote add ${repoName} ${remoteUrl}")
+      sh("git remote -v | grep -q ${repoName} || git remote add ${repoName} ${remoteUrl}")
       sh("git -c core.askpass=true push ${repoName}")
     }
 }
