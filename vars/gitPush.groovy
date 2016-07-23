8aaec2f379751fdd308830ec27f5c2eb9b383942
Sofer Athlan-Guyot <chem@sathlan.org>
2016-07-23 17:37:59 +0200

    Add github.com to knwon_hosts.

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -1,5 +1,6 @@
 def call(String remoteUrl, String branch, String credentialsId, String repoName = 'origin') {
     sshagent([credentialsId]) {
+      sh("ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts")
       sh("git remote add ${repoName} ${remoteUrl}")
       sh("git -c core.askpass=true push ${repoName}")
     }
