30223de027f34d92ea165748ae9fafda6489b89f
Sofer Athlan-Guyot <chem@sathlan.org>
2016-08-05 13:04:42 +0200

    Improve runBeaker.

--- vars/runBeaker.groovy
+++ vars/runBeaker.groovy
@@ -1,10 +1,11 @@
 def call(String project, String host, String user = 'root') {
   def tpl_file = '/home/vagrant/Vagrantfile.tpl'
+  def tpl_file_dest = "spec/acceptance/nodesets/centos-puppet-#{project}.yml"
+  sh("cp ${tpl_file} ${tpl_file_dest}")
   sh("echo \"gem 'beaker', :git => 'https://github.com/sathlan/beaker.git', :branch => 'feature/libvirt_options'\" >> Gemfile")
   sh('bundle install --binstubs')
-  sh("sed -i -e 's/%MODULE%/${project}/' ${tpl_file}")
-  sh("sed -i -e 's/%USER%/${user}/' ${tpl_file}")
-  sh("sed -i -e 's/%HOST%/${host}/' ${tpl_file}")
-  sh("cp ${tpl_file} spec/acceptance/nodesets/centos-puppet-${project}.yml")
+  sh("sed -i -e 's/%MODULE%/${project}-${env.BUILD_ID}/' ${tpl_file_dest}")
+  sh("sed -i -e 's/%USER%/${user}/' ${tpl_file_dest}")
+  sh("sed -i -e 's/%HOST%/${host}/' ${tpl_file_dest}")
   sh("env BEAKER_destroy=no BEAKER_set=centos-puppet-${project} BEAKER_debug=yes ./bin/rake beaker")
 }
