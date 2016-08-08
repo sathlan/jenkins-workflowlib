a2aca8c67fff79cfb9bc38ae378a7e0deee0fa9a
Sofer Athlan-Guyot <chem@sathlan.org>
2016-08-08 16:24:31 +0200

    Add notification to stageCloud

--- vars/stageCloud.groovy
+++ vars/stageCloud.groovy
@@ -1,6 +1,10 @@
 def call(String desc, body) {
   stage "${desc}"
+  node ('local') {
+    matterMost 'good', "${env.BRANCH_NAME}: ${desc} (${env.BUILD_URL}/console)"
+  }
   y = askForNextStep "${desc} ?"
+
   if (y) {
     node ('local') {
       sshagent(['jenkins']) {
