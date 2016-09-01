ff3f4e64287da61f45fac4a45cddf4e127734588
Sofer Athlan-Guyot <chem@sathlan.org>
2016-09-01 12:21:29 +0200

    Notify before blocking.

--- vars/askForNextStep.groovy
+++ vars/askForNextStep.groovy
@@ -1,5 +1,8 @@
 def call(String msg) {
   if (!env.DONT_ASK) {
+    node ('local') {
+      matterMost 'good', "${env.BRANCH_NAME}: Asking about ${msg} (${env.BUILD_URL}/console)"
+    }
     input message: msg, parameters: [[$class: 'BooleanParameterDefinition', defaultValue: false, description: '', name: 'YES' ]]
   }
 }
