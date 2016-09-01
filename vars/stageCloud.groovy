36708a3d84592cf96d67fe66273e8ac3736bdd4c
Sofer Athlan-Guyot <chem@sathlan.org>
2016-09-01 12:21:45 +0200

    Remove useless notification.

--- vars/stageCloud.groovy
+++ vars/stageCloud.groovy
@@ -1,8 +1,5 @@
 def call(String desc, body) {
   stage "${desc}"
-  node ('local') {
-    matterMost 'good', "${env.BRANCH_NAME}: ${desc} (${env.BUILD_URL}/console)"
-  }
   y = askForNextStep "${desc} ?"
 
   if (y) {
