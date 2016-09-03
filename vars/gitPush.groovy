b4005f200e68a62bc9c41e9325fbf765a793bf21
Sofer Athlan-Guyot <chem@sathlan.org>
2016-09-03 18:00:39 +0200

    Improve gitPush.

--- vars/gitPush.groovy
+++ vars/gitPush.groovy
@@ -1,8 +1,7 @@
 // https://github.com/jenkinsci/workflow-cps-global-lib-plugin
-def call(String remoteUrl, String branch, String credentialsId, String repoName = 'origin') {
+def call(String remoteUrl, String credentialsId, String repoName = 'origin') {
     sshagent([credentialsId]) {
-      sh("grep -q github.com ~/.ssh/known_hosts || ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts")
-      sh("git remote -v | grep -q ${repoName} || git remote add ${repoName} ${remoteUrl}")
+      addSshHostEntry(remoteUrl)
       sh("git -c core.askpass=true push ${repoName}")
     }
 }
