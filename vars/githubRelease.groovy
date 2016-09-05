b40143780bcd620e74201571b8596b63365f0e69
Sofer Athlan-Guyot <chem@sathlan.org>
2016-09-05 03:33:02 +0200

    Use the bundler git-release command.

--- vars/githubRelease.groovy
+++ vars/githubRelease.groovy
@@ -2,6 +2,6 @@ def call(String version) {
   sh("git config --global release.remote github")
   withCredentials([[$class: 'StringBinding', credentialsId: 'github-api-token', variable: 'GITHUB_API_TOKEN']]) {
     sh("git config --global release.api-token ${env.GITHUB_API_TOKEN}")
-    sh("git release")
+    sh("git-release")
   }
 }
