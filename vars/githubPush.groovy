0a12b0dbce8dd63ce2bc4bb28397dab891fa1ee8
Sofer Athlan-Guyot <chem@sathlan.org>
2016-09-06 20:14:40 +0200

    Fix branch detection.

--- vars/githubPush.groovy
+++ vars/githubPush.groovy
@@ -3,7 +3,7 @@ def call(String credentialsId, String repoName = 'origin') {
       addSshHostEntry('github.com')
       repo =  env.JOB_NAME.split("/")[0]
       branch =  env.JOB_NAME.split("/")[1]
-      sh("! git branch -a | grep ${branch} || git branch -D ${branch}")
+      sh("! git branch -a | grep '^ *${branch}\$' || git branch -D ${branch}")
       sh("git checkout -b ${branch}")
       sh("git remote | grep github || git remote add github ${env.GITHUB_ACCOUNT}/${repo}.git")
       sh("git push github ${branch}")
