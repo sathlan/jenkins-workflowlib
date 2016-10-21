876d84606e8d010fd84aba58f1ff19398de380fd
Sofer Athlan-Guyot <chem@sathlan.org>
2016-10-21 14:37:49 +0200

    [gitCommit] remove getOutput hack.

--- vars/gitEnv.groovy
+++ vars/gitEnv.groovy
@@ -1,7 +1,7 @@
 // https://github.com/jenkinsci/workflow-cps-global-lib-plugin
 def call() {
   if (env.GIT_COMMIT == null) {
-    def gitCommit = getOutput('git rev-parse HEAD').trim()
+    def gitCommit = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
     // short SHA, possibly better for chat notifications, etc.
     def gitShortCommit = gitCommit.take(6)
     env.GIT_COMMIT = gitCommit
