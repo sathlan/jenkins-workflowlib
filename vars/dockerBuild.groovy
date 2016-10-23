6c266d1ce8cfb4766213119261ce692f452df7b0
Sofer Athlan-Guyot <chem@sathlan.org>
2016-10-23 02:50:37 +0200

    [dockerBuild] adding timestamp and app name.

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,8 @@
 def call(String dockerName, Boolean isSystemd = true, Boolean isApp = false, Boolean needPuppet = false, Boolean isPublic = false) {
+  if (isApp) {
+    dockerName = "app-${dockerName}"
+  }
+
   def dockerOpt = ''
   def currentDir = "${pwd()}"
 
@@ -17,85 +21,87 @@ def call(String dockerName, Boolean isSystemd = true, Boolean isApp = false, Boo
   def buildImage = "${imageName}:${env.BUILD_ID}"
   def myEnv
 
-  stage('Checkout') {
-    checkout scm
-  }
-
-  ansiColor() {
-    stage('Prepare env') {
-      withEnv(rubyEnv) {
-        sh "[ ! -e Gemfile.lock ] || rm Gemfile.lock"
-        sh "bundle install --binstubs"
-      }
+  timestamps {
+    stage('Checkout') {
+      checkout scm
     }
 
-    docker.withRegistry("${env.DOCKER_PRV_REGISTRY}", 'docker-private-registry') {
-      stage('build image') {
-        myEnv = docker.build "${imageName}:${env.BUILD_ID}", "${dockerBuildOpt}"
+    ansiColor() {
+      stage('Prepare env') {
+        withEnv(rubyEnv) {
+          sh "[ ! -e Gemfile.lock ] || rm Gemfile.lock"
+          sh "bundle install --binstubs"
+        }
       }
 
-      if (needPuppet) {
-        stage('puppet configure') {
-          // detect if puppet files have changed
-          def changed_files = sh(returnStdout: true, script: "git diff-tree --no-commit-id --name-only -r ${env.GIT_COMMIT}")
-          // Empty list is the initial commit.
-          if (changed_files =~ /(?m)^puppet/ || changed_files.allWhitespace) {
-            docker.image(remote_puppet_image).withRun("${dockerOpt} -v /opt/puppetlabs") {p ->
-              myEnv.withRun("${dockerOpt} --volumes-from ${p.id} -e PATH=/opt/puppetlabs/bin:${env.PATH} -v ${currentDir}:${currentDir}:z --entrypoint /usr/sbin/init -u 0") {m ->
-                sh "docker exec -t ${m.id} puppet module install --modulepath ${currentDir}/puppet/modules --target-dir ${currentDir}/puppet/modules puppetlabs-postgresql"
-                sh "docker exec -t ${m.id} puppet apply --debug --modulepath ${currentDir}/puppet/modules ${currentDir}/puppet/manifest.pp"
-                sh "docker commit -m 'Puppet configuration' ${m.id} ${buildImage}"
+      docker.withRegistry("${env.DOCKER_PRV_REGISTRY}", 'docker-private-registry') {
+        stage('build image') {
+          myEnv = docker.build "${imageName}:${env.BUILD_ID}", "${dockerBuildOpt}"
+        }
+
+        if (needPuppet) {
+          stage('puppet configure') {
+            // detect if puppet files have changed
+            def changed_files = sh(returnStdout: true, script: "git diff-tree --no-commit-id --name-only -r ${env.GIT_COMMIT}")
+            // Empty list is the initial commit.
+            if (changed_files =~ /(?m)^puppet/ || changed_files.allWhitespace) {
+              docker.image(remote_puppet_image).withRun("${dockerOpt} -v /opt/puppetlabs") {p ->
+                myEnv.withRun("${dockerOpt} --volumes-from ${p.id} -e PATH=/opt/puppetlabs/bin:${env.PATH} -v ${currentDir}:${currentDir}:z --entrypoint /usr/sbin/init -u 0") {m ->
+                  sh "docker exec -t ${m.id} puppet module install --modulepath ${currentDir}/puppet/modules --target-dir ${currentDir}/puppet/modules puppetlabs-postgresql"
+                  sh "docker exec -t ${m.id} puppet apply --debug --modulepath ${currentDir}/puppet/modules ${currentDir}/puppet/manifest.pp"
+                  sh "docker commit -m 'Puppet configuration' ${m.id} ${buildImage}"
+                }
               }
             }
           }
         }
-      }
 
-      stage('publish image') {
-        myEnv.withRun(dockerOpt) {c ->
-          sh "rm -rf artifacts && mkdir -p artifacts"
-          sh "docker inspect ${c.id} > artifacts/inspect.txt"
-          sh "docker history ${buildImage} > artifacts/history.txt"
-        }
-      }
-
-      stage('test image') {
-        myEnv.withRun("${dockerOpt}") {c ->
-          withEnv(rubyEnv + ["${dockerName.replaceAll('-','_').toUpperCase()}_ID=${c.id}", "LOCALHOST_ID=${c.id}", "DOCKER_IMAGE=${buildImage}"]) {
-            sh "rake spec:${dockerName}"
-            sh "rake spec:localhost"
+        stage('publish image') {
+          myEnv.withRun(dockerOpt) {c ->
+            sh "rm -rf artifacts && mkdir -p artifacts"
+            sh "docker inspect ${c.id} > artifacts/inspect.txt"
+            sh "docker history ${buildImage} > artifacts/history.txt"
           }
         }
 
-        if (!isApp.isAllWhitespace()) {
-          METASPLOIT_APP_VERSION
-          def dockerNameUp = appName.upcase()
-          def appVersion = "${dockerNameUp}_APP_VERSION"
-          def appDir = "${dockerNameUp}_APP_DIR"
-          withEnv(["${appVersion}=${env.BUILD_ID}", "${appDir}=${currentDir}/spec/fixture"]) {
-            sh "env"
-            sh "bash -x ./bin/app-${dockerName} --version"
+        stage('test image') {
+          myEnv.withRun("${dockerOpt}") {c ->
+            withEnv(rubyEnv + ["${dockerName.replaceAll('-','_').toUpperCase()}_ID=${c.id}", "LOCALHOST_ID=${c.id}", "DOCKER_IMAGE=${buildImage}"]) {
+              sh "rake spec:${dockerName}"
+              sh "rake spec:localhost"
+            }
+          }
+
+          if (!isApp.isAllWhitespace()) {
+            METASPLOIT_APP_VERSION
+            def dockerNameUp = appName.upcase()
+            def appVersion = "${dockerNameUp}_APP_VERSION"
+            def appDir = "${dockerNameUp}_APP_DIR"
+            withEnv(["${appVersion}=${env.BUILD_ID}", "${appDir}=${currentDir}/spec/fixture"]) {
+              sh "env"
+              sh "bash -x ./bin/app-${dockerName} --version"
+            }
           }
         }
-      }
 
-      stage('promote') {
-        myEnv.push('latest')
-        def tag = sh returnStdout: true, script: 'git describe --exact-match HEAD || true'
-        tag = tag.trim()
-        if (tag =~ /^v.*/) {
-          myEnv.push(tag)
-          sh "echo ${tag} > artifacts/RELEASE"
-        }
-        archive 'artifacts/'
-        if(isPublic) {
-          timeout(time: 1, unit: 'DAYS') {
-            if (askForNextStep('Deploy to GitHub ?')) {
-              lock("githubPush-docker-${dockerName}") {
-                githubPush('jenkins', 'github')
-                if (askForNextStep('Release to GitHub ?')) {
-                  withEnv(rubyEnv) {
-                    githubRelease()
+        stage('promote') {
+          myEnv.push('latest')
+          def tag = sh returnStdout: true, script: 'git describe --exact-match HEAD || true'
+          tag = tag.trim()
+          if (tag =~ /^v.*/) {
+            myEnv.push(tag)
+            sh "echo ${tag} > artifacts/RELEASE"
+          }
+          archive 'artifacts/'
+          if(isPublic) {
+            timeout(time: 1, unit: 'DAYS') {
+              if (askForNextStep('Deploy to GitHub ?')) {
+                lock("githubPush-docker-${dockerName}") {
+                  githubPush('jenkins', 'github')
+                  if (askForNextStep('Release to GitHub ?')) {
+                    withEnv(rubyEnv) {
+                      githubRelease()
+                    }
                   }
                 }
               }
