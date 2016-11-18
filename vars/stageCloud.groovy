10ca01721ea447ee6229e8101bcf53ed8d41b14f
Sofer Athlan-Guyot <chem@sathlan.org>
2016-11-18 12:29:25 +0100

    [stageCloud] user env.SSH_CREDENTIAL_ID for ssh key.

--- vars/stageCloud.groovy
+++ vars/stageCloud.groovy
@@ -4,7 +4,7 @@ def call(String desc, body) {
 
     if (y) {
       node ('local') {
-        sshagent(['jenkins']) {
+        sshagent([env.SSH_CREDENTIAL_ID]) {
           ansiColor() {
             body()
           }
