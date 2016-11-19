1acbc8db5fdfc6582a7a028d1651e35f644654dc
Sofer Athlan-Guyot <chem@sathlan.org>
2016-11-19 12:11:06 +0100

    [askForNextStep] Make all parameter available.

--- vars/askForNextStep.groovy
+++ vars/askForNextStep.groovy
@@ -1,10 +1,10 @@
-def call(String msg) {
+def call(String msg, String description='', String color='good') {
   def answer = false
   if (env.DONT_ASK != 'true' && env.DONT_ASK != true) {
     node ('local') {
-      matterMost 'good', "${env.BRANCH_NAME}: Asking about ${msg} (${env.BUILD_URL}/console)"
+      matterMost(color, "${env.BRANCH_NAME}: Asking about ${msg} (${env.BUILD_URL}/console)")
     }
-    answer = input message: msg, parameters: [[$class: 'BooleanParameterDefinition', defaultValue: false, description: '', name: 'YES' ]]
+    answer = input message: msg, parameters: [[$class: 'BooleanParameterDefinition', defaultValue: false, description: description, name: 'YES' ]]
   } else {
     answer = true
   }
